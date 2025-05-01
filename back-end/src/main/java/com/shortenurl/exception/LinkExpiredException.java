05a048315ce7262eca9fae8d68536cccaacb220d
Youngjin Mo <dev.youngjinmo@gmail.com>
2025-05-01 16:23:06 +0900

    front/server 예외 핸들링

--- back-end/src/main/java/com/shortenurl/exception/LinkExpiredException.java
+++ back-end/src/main/java/com/shortenurl/exception/LinkExpiredException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public class LinkExpiredException extends CustomException{
     public LinkExpiredException() {
-        super(HttpStatus.BAD_REQUEST, "Link has expired.");
+        super(HttpStatus.BAD_REQUEST, "Link expired");
     }
 }
